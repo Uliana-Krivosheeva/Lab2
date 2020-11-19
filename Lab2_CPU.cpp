171a645b6fe891b024349157e9c648df666e9d4e
Uliana-Krivosheeva <73125843+Uliana-Krivosheeva@users.noreply.github.com>
2020-11-19 12:46:54 +0400

    Add files via upload

---
+++ Lab2_CPU.cpp
@@ -0,0 +1,246 @@
+﻿#include <iostream>
+#include <cmath>
+#include <chrono>
+
+
+#define pi 3.1415926535
+#define N 4 //количество  коэффициентов
+
+
+using namespace std;
+
+
+
+void genFirst(double* ind, int countParents) { //Функция генерации первого(случайного) поколения
+
+	for (int i = 0; i < countParents; i++) {
+		for (int n = 0; n < N; n++) {
+			if (rand() / static_cast<double>(RAND_MAX) > 0.5)
+				ind[i * N + n] = rand() / static_cast<double>(RAND_MAX);
+			else
+				ind[i * N + n] = -rand() / static_cast<double>(RAND_MAX);
+		}
+	}
+}
+
+int getRandomNumber(int min, int max) // Функция для генерации равномернораспределенного случайного числа
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+
+void fitness(int count, int countInd, double* childrens, double* arrInd) { // Функция вычисления отклонения(считаем квадрат отклонения от синуса и помещаем в массив ошибок)
+
+	double faterr = 0.0;
+	double err;
+	double h = 2.0 * pi / count;
+	double X;
+
+	for (int i = 0; i < countInd; i++) {
+
+		for (int j = 0; j < count; j++) {
+			err = 0.0;
+			for (int k = 0; k < N; k++) {
+				X = pow(j * h + h, k);
+				err += childrens[i * N + k] * X;
+			}
+			faterr += pow(sin(j * h + h) - err, 2);
+		}
+
+		arrInd[i] = faterr;
+		faterr = 0.0;
+	}
+
+}
+
+void selectBestParents(int* indexes, double* arr, int countIndividuals, int countParents, double* bP, double* childrens) { // Функция отбора(по массиву из ошибок arr выбираем индексы и кладем "участников" отбора с этими индексами в массив bP)
+
+	double* arr_min = (double*)malloc(countParents * sizeof(double));
+
+	for (int p = 0; p < countParents; p++) {
+		arr_min[p] = DBL_MAX;
+	}
+
+	for (int i = 0; i < countIndividuals; i++) {
+		if (arr[i] < arr_min[0]) {
+			arr_min[0] = arr[i];
+			indexes[0] = i;
+		}
+	}
+
+	for (int p = 1; p < countParents; p++) {
+		for (int i = 0; i < countIndividuals; i++) {
+			if (arr[i] > arr_min[p - 1] && arr[i] < arr_min[p]) {
+				arr_min[p] = arr[i];
+				indexes[p] = i;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < countParents; i++) {
+		for (int j = 0; j < N; j++) {
+			bP[i * N + j] = childrens[indexes[i] * N + j];
+		}
+
+	}
+
+}
+
+void crossover(double* parent1, double* parent2, double* child) { //Функция скрещевания родителей
+
+	int n = getRandomNumber(0, N);
+	for (int i = 0; i < n; i++) {
+		child[i] = parent1[i];
+	}
+
+	for (int i = n; i < N; i++) {
+		child[i] = parent2[i];
+	}
+
+}
+
+void mutation(double* child, double Em, double Dm) { // Функция мутации
+
+	int n = getRandomNumber(0, N);
+	double d = Dm * std::rand() / 32767.0;
+	double m = Em;
+	if (rand() / 32767.0 > 0.5)
+		m += d;
+	else
+		m -= d;
+
+	if (rand() / 32767.0 > 0.5)
+		child[n] += m;
+	else
+		child[n] -= m;
+}
+
+void makeChildren(double* parents, double* childrens, int countParents, int countInd, double Em, double Dm) { //Функция создания ребенка на основе скрещивания и случайной мутации
+
+	double* child;
+	double* p1;
+	double* p2;
+
+	p1 = (double*)malloc(N * sizeof(double));
+	p2 = (double*)malloc(N * sizeof(double));
+	child = (double*)malloc(N * sizeof(double));
+
+	for (int i = 0; i < countParents; i++) {// Выбираем родителей для будущих детей
+		int l = 0;
+		for (int k = 0; k < N; k++) {
+			p1[k] = parents[i * N + k];
+			p2[k] = parents[(countParents - i) * N + k];
+			l++;
+		}
+		int temp = countInd / countParents;
+		for (int j = 0; j < temp; j++) {
+
+			crossover(p1, p2, child);
+
+			if (rand() / 32767.0 > 0.5) {
+				mutation(child, Em, Dm);
+			}
+
+			for (int k = 0; k < N; k++) {
+				childrens[i*temp + j * N + k] = child[k];
+			}
+		}
+	}
+}
+
+
+
+int main()
+{
+	int count, countInd, countParents, maxIter, maxConstIter;
+	double Em, Dm;
+
+	cout << "Enter count of points (500 - 1000): " << endl;
+	cin >> count;
+
+	cout << "Enter count of individuals (1000 - 2000): " << endl;
+	cin >> countInd;
+
+	cout << "Enter mean for Mutation: " << endl;
+	cin >> Em;
+
+	cout << "Enter varience for Mutation: " << endl;
+	cin >> Dm;
+
+	cout << "Enter count pair of parents: " << endl;
+	cin >> countParents;
+
+	countParents *= 2;
+
+	cout << "Enter maximal count of epochs: " <<endl;
+	cin >> maxIter;
+
+	cout << "Enter maximal count of epochs with same results: " << endl;
+	cin >> maxConstIter;
+
+
+	double* bP = (double*)malloc(countParents * N * sizeof(double));
+
+	genFirst(bP, countParents); // Создаем первое поколение
+
+
+	int* indexes = (int*)malloc(countParents * sizeof(int));
+	double* arrInd = (double*)malloc(countInd * sizeof(double));
+	double* childrens = (double*)malloc(countInd * N * sizeof(double));
+
+	double min = DBL_MAX, val = DBL_MAX;
+	int indBest, sameIter = 1;
+	int sumTime = 0;
+	
+	for (int epoch = 1; epoch <= maxIter; epoch++) { //Начинаем эволюцию!!!!
+
+		auto begin = chrono::steady_clock::now();
+
+		makeChildren(bP, childrens, countParents, countInd, Em, Dm);
+
+		fitness(count, countInd, childrens, arrInd);
+
+		selectBestParents(indexes, arrInd, countInd, countParents, bP, childrens);
+
+		auto end = chrono::steady_clock::now();
+		auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+		sumTime += elapsed_ms.count();
+		cout << epoch << endl;
+		if (arrInd[indexes[0]] < min) {
+			min = arrInd[indexes[0]];
+			indBest = epoch;
+		}
+
+		if (val == arrInd[indexes[0]]) {
+			sameIter++;
+		}
+
+		else {
+			val = arrInd[indexes[0]];
+			sameIter = 1;
+		}
+
+		if (sameIter >= maxConstIter) {
+			cout << "Same " << maxConstIter << " iterations" << endl;
+			break;
+		}
+	}
+
+
+	cout << "time: " << sumTime << endl;
+    cout << "min: " << min << endl << "epoch: " << indBest << endl;
+
+	//double* temp = (double*)malloc(N * sizeof(double));
+	cout << "Results: " << endl;
+	for (int j = 0; j < N; j++) {
+		cout << bP[j] << " ";
+		
+	}
+	cout <<endl;
+	//free(bP);
+
+}
+
+
---
+++ Lab2_GPU.cu
@@ -0,0 +1,260 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <curand_kernel.h>
+
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+
+#include <stdio.h>
+#include <cmath>
+#include <float.h>
+#include <chrono>
+#include <iostream>
+
+#define BLOCK_SIZE 16
+#define pi 3.1415926535
+#define N 4 
+
+
+using namespace std;
+using namespace thrust;
+
+
+
+void fitness(int count, int countInd, double* childrens, double* arrInd) {
+
+	double faterr = 0.0;
+	double err;
+	double h = pi / count;
+	double X;
+
+	for (int i = 0; i < countInd; i++) {
+
+		for (int j = 0; j < count; j++) {
+			err = 0.0;
+			for (int k = 0; k < N; k++) {
+				X = pow(j * h + h, k);
+				err += childrens[i * N + k] * X;
+			}
+			faterr += pow(sin(j * h + h) - err, 2);
+		}
+
+		arrInd[i] = faterr;
+		faterr = 0.0;
+	}
+
+
+}
+
+void selectBestParents(int* indexes, double* arrInd, int countInd, int countParents, double* bP, double*childrens) {
+
+	for (int i = 0; i < countInd; i++) {
+		indexes[i] = i;
+	}
+
+	sort_by_key(arrInd, arrInd + countInd, indexes);
+
+	for (int i = 0; i < countParents; i++) {
+		for (int j = 0; j < N; j++) {
+			bP[i * N + j] = childrens[N * indexes[i] + j];
+		}
+	}
+
+}
+
+
+__global__ void MakeChildrens_GPU(int Em, int Dm, int count, int countInd, double* d_bP, double* d_arrInd, double* d_childrens) {
+
+	int countParents = 10;
+
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+
+	if (index < countParents) {
+		for (int i = index; i < countParents; i += stride) {
+			int countChildren = countInd / countParents;
+			for (int j = 0; j < countChildren; j++) {
+
+				curandState state;
+				curand_init((unsigned long long)clock() + index, 0, 0, &state);
+
+				int n = floor(curand_uniform_double(&state) * N);
+
+				
+
+				for (int k = 0; k < n; k++) {
+					d_childrens[i * countChildren * N + j * N + k] = d_bP[i * N + k];
+				}
+
+				for (int k = n; k < N; k++) {
+					d_childrens[i * countChildren * N + j * N + k] = d_bP[(countParents - i) * N + k];
+				}
+				curand_init((unsigned long long)clock() + index, 0, 0, &state);
+
+				if (curand_uniform_double(&state) > 0.5) {
+
+					double d = Dm * curand_uniform_double(&state);
+					double m = Em;
+					if (curand_uniform_double(&state) > 0.5)
+						m += d;
+					else
+						m -= d;
+					int nn = (int)(curand_uniform_double(&state) * N);
+					if (curand_uniform_double(&state) > 0.5)
+						d_childrens[i * countChildren * N + j * N + nn] += m;
+					else
+						d_childrens[i * countChildren * N + j * N + nn] -= m;
+				}
+			}
+		}
+	}
+
+}
+
+
+int main()
+{
+
+	int count, countInd, countParents, maxIter, maxConstIter;
+	double Em, Dm;
+	
+
+	countParents = 10;
+
+	cout << "Enter count of points (500 - 1000): " << endl;
+	cin >> count;
+
+	cout << "Enter count of individuals (1000 - 2000): " << endl;
+	cin >> countInd;
+
+	cout << "Enter mean for Mutation: " << endl;
+	cin >> Em;
+
+	cout << "Enter varience for Mutation: " << endl;
+	cin >> Dm;
+
+	cout << "Enter maximal count of epochs: " << endl;
+	cin >> maxIter;
+
+	cout << "Enter maximal count of epochs with same results: " << endl;
+	cin >> maxConstIter;
+
+	
+
+
+	double* h_bP = new double[countParents * N];
+	for (int i = 0; i < countParents; i++) {
+		for (int j = 0; j < N; j++) {
+			h_bP[i * N + j] = 0.7;
+		}
+	}
+
+	
+	int* h_indexes = new int[countInd];
+	double* h_arrInd = new double[countInd];
+	double* h_childrens = new double[countInd * N];
+
+
+
+	for (int i = 0; i < countInd; i++) {
+		for (int j = 0; j < N; j++) {
+			h_childrens[i * N + j] = 0.0;
+		}
+	}
+
+
+	int* d_indexes;
+	double* d_arrInd;
+	double* d_bP;
+	double* d_childrens;
+	double min = DBL_MAX, val = DBL_MAX;
+	int indBest, sameIter = 1;
+
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	float gpu_elapsed_time_ms;
+	float sumTime = 0.0;
+
+
+	cudaMalloc((void**)&d_arrInd, countInd * sizeof(double));
+	cudaMalloc((void**)&d_bP, countParents * N * sizeof(double));
+	cudaMalloc((void**)&d_childrens, countInd * N * sizeof(double));
+
+
+	for (int epoch = 1; epoch <= maxIter; epoch++) {
+
+
+		cudaEventRecord(start, 0);
+		cudaMemcpy(d_bP, h_bP, countParents * N * sizeof(double), cudaMemcpyHostToDevice);
+
+		MakeChildrens_GPU << <countParents * countInd, 1 >> > (Em, Dm, count, countInd, d_bP, d_arrInd, d_childrens);
+
+		cudaEventRecord(stop, 0);
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
+
+		sumTime += gpu_elapsed_time_ms;
+
+
+		cudaMemcpy(h_childrens, d_childrens, countInd * N * sizeof(double), cudaMemcpyDeviceToHost);
+		auto begin = chrono::steady_clock::now();
+
+		fitness(count, countInd, h_childrens, h_arrInd);
+		selectBestParents(h_indexes, h_arrInd, countInd, countParents, h_bP, h_childrens);
+
+		auto end = chrono::steady_clock::now();
+
+		auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+
+		sumTime += elapsed_ms.count();
+		cout << epoch << endl;
+
+
+		if (h_arrInd[0] < min) {
+			min = h_arrInd[0];
+			indBest = epoch;
+		}
+
+		if (val == h_arrInd[0]) sameIter++;
+
+		else {
+			val = h_arrInd[0];
+			sameIter = 1;
+		}
+
+		if (sameIter >= maxConstIter) {
+			cout << "Same " << maxConstIter << " iterations" << endl;
+			break;
+		}
+
+	}
+
+
+	cout<< "Time: " << sumTime << endl;
+
+	//cout << "min: " << min << endl << "epoch: " << indBest << endl;
+
+	double* temp = (double*)malloc(N * sizeof(double));
+	cout << "Result: " << endl;
+	for (int j = 0; j < N; j++) {
+		cout << h_bP[j] << " ";
+	}
+	cout << endl;
+
+	cudaFree(d_arrInd);
+	cudaFree(d_bP);
+	cudaFree(d_childrens);
+
+
+}
+
+
+
+
+
+
+
